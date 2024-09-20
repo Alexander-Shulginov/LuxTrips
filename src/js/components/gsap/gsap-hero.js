ef069619a16c0dc9c5d21b27afa9a7913d0d4984
Alexander <alexander3post@gmail.com>
2024-09-20 15:34:02 +0300

    fix hover menu

--- src/js/components/gsap/gsap-hero.js
+++ src/js/components/gsap/gsap-hero.js
@@ -1,65 +1,47 @@
 import { gsap } from 'gsap';
-
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
+const defaultParams = {
+	duration: 1.4,
+	// delay: 2.6,
+	delay: 0,
+};
+
 export default function gsapHero() {
-	gsap.timeline()
+	gsap.timeline({ defaults: defaultParams })
 		.from(['.hero__img-2', '.hero__subtitle'], {
 			right: -550,
 			opacity: 0,
-			duration: 1.4,
-			delay: 2.6,
 		}, 0)
 		.from('.hero__title', {
 			left: -850,
 			opacity: 0,
-			duration: 1.4,
-			delay: 2.6,
 		}, 0)
 		.from('.hero__text', {
 			bottom: -150,
 			opacity: 0,
-			duration: 1.4,
-			delay: 2.6,
 		}, 0)
 		.to(['.hero__img-2', '.hero__subtitle'], {
 			right: 0,
 			opacity: 1,
-			duration: 1.4,
-			delay: 2.6,
 		}, 0)
 		.to('.hero__img-1', {
 			scale: 1.05,
 			opacity: 1,
-			duration: 1.4,
-			delay: 2.6,
-		}, 0)
-		.to('.hero__title', {
-			left: 0,
-			opacity: 1,
-			duration: 1.4,
-			delay: 2.6,
 		}, 0);
 
-	gsap.timeline()
+	gsap.timeline({ defaults: defaultParams })
 		.from('.hero__rhomb', {
 			top: 45,
 			scale: 0,
-			duration: 1.4,
-			delay: 2.6,
 		}, 1.3)
 		.from(['.hero__title', '.hero__subtitle'], {
 			scale: 0.8,
-			opacity: 1,
-			duration: 1.4,
-			delay: 2.6,
 		}, 1.3)
 		.to(['.hero__title', '.hero__subtitle'], {
 			scale: 1,
 			opacity: 1,
-			duration: 1.4,
-			delay: 2.6,
 		}, 1.3);
 }
