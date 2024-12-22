ed8930abb390b669ab3deb4164bdf1b6928f4de6
Alexander_721 <alexander3post@gmail.com>
2024-12-22 21:14:39 +0300

    update contact section

--- src/js/components/gsap/gsap-footer.js
+++ src/js/components/gsap/gsap-footer.js
@@ -6,27 +6,15 @@ gsap.registerPlugin(ScrollTrigger);
 export default function gsapFooter() {
 	const tl = gsap.timeline();
 
-	tl.from('.footer__decor', {
-		scrollTrigger: {
-			trigger: '.footer__logo',
-			// start: 'center',
-			// end: '150%',
-			// markers: true,
-		},
-		opacity: 0,
-		delay: 0.5,
-		duration: 1.5,
-	});
-
-	tl.from('.footer__info', {
-		scrollTrigger: {
-			trigger: '.footer__info',
-			// start: 'top',
-			// end: '50%',
-			// markers: true,
-		},
-		opacity: 0,
-		delay: 0.5,
-		duration: 1,
-	});
+	// tl.from('.footer__logo', {
+	// 	scrollTrigger: {
+	// 		trigger: '.footer',
+	// 		start: '-10%',
+	// 		end: '50%',
+	// 		// markers: true,
+	// 	},
+	// 	opacity: 0,
+	// 	// delay: 0.5,
+	// 	duration: 1,
+	// });
 }
